a9ce6f44167f1afb160bcf514329b115a7f28293
Seeeelim <mohamedselim.harzallah@esprit.tn>
2025-02-14 01:19:27 +0100

    Interface

--- src/main/java/esprit/tn/controllers/AfficherFeedbackController.java
+++ src/main/java/esprit/tn/controllers/AfficherFeedbackController.java
@@ -12,14 +12,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import org.w3c.dom.Text;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class AfficherFeedbackController {
 
@@ -45,6 +48,9 @@ public class AfficherFeedbackController {
     private TableColumn<Feedback, Integer> ReclamationId;
 
     @FXML
+    private Button backReclamation;
+
+    @FXML
     void initialize() {
         FeedbackService fs = new FeedbackService();
 
@@ -57,6 +63,30 @@ public class AfficherFeedbackController {
         noteF.setCellValueFactory(new PropertyValueFactory<>("Note"));
         dateCreationF.setCellValueFactory(new PropertyValueFactory<>("DateFeedback"));
         ReclamationId.setCellValueFactory(new PropertyValueFactory<>("ReclamationId"));
+    }
+
+    @FXML
+    void BackReclamation(ActionEvent event) {
+        // Fermer la fenêtre de modification sans enregistrer les modifications
+        backReclamation.getScene().getWindow().hide();
+
+        try {
+            // Charger le fichier FXML de la page AjouterReclamation
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/AfficherReclamation.fxml")));
+            Scene scene = new Scene(root);
+
+            // Obtenir la fenêtre actuelle (stage) à partir de l'événement
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+
+            // Définir la nouvelle scène sur le stage
+            stage.setScene(scene);
+            stage.show(); // Afficher la nouvelle scène
+
+        } catch (IOException e) {
+            // Gérer les erreurs de chargement du FXML
+            e.printStackTrace();
+            System.err.println("Erreur lors du chargement de AjouterReclamation.fxml");
+        }
     }
 
     @FXML
