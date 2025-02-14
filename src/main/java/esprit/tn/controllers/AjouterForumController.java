bec0d49d66d2ca1cf34c6d5702017fd196b9c104
Hedi <ElKamel.MohamedHedi@esprit.tn>
2025-02-14 18:34:50 +0100

    interface Add Forum

---
+++ src/main/java/esprit/tn/controllers/AjouterForumController.java
@@ -0,0 +1,24 @@
+package esprit.tn.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class AjouterForumController {
+
+    @FXML
+    private TextField forumAuthorId;
+
+    @FXML
+    private TextArea forumDescription;
+
+    @FXML
+    private TextField forumTitle;
+
+    @FXML
+    void AjouteForum(ActionEvent event) {
+
+    }
+
+}
